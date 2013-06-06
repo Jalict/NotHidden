7460f1e4573810512d1a389762dfe44f47a7a517
Rasmus Dyhr Larsen <ahhhnimal@gmail.com>
2013-06-07 01:17:14 +0200

    Upload

---
+++ Unity/Assets/Scripts/CustomController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class CustomController : MonoBehaviour {
+	
+	private Camera cam;
+	private Vector3 face;
+
+	// Use this for initialization
+	void Start () {
+		cam = transform.GetComponentInChildren<Camera>();
+		Debug.Log(cam);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Rotate player
+		
+		
+		// Move player
+		
+		
+		// Message actions
+	}
+}
