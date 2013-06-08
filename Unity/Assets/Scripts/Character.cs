4d13a23a8a34c96141bac733e0aaa553c8c5807f
Rasmus Dyhr Larsen <ahhhnimal@gmail.com>
2013-06-09 01:09:53 +0200

    Basic Hunter Jump
    Also tried out lightmaps. Wall clinging coming up soonish.

--- Unity/Assets/Scripts/Character.cs
+++ Unity/Assets/Scripts/Character.cs
@@ -2,12 +2,30 @@ using UnityEngine;
 using System.Collections;
 
 public class Character : MonoBehaviour {
+	public string jumpKey = "Fire2";
+	public float jumpForce = 40;
+	
+	private Vector3 middleScreen;
+	private CharacterMotor motor;
+	private bool hanging;
 
 	void Start () {
-	
+		middleScreen = new Vector3(Screen.width/2,Screen.height/2,0);
+		motor = GetComponentInChildren<CharacterMotor>();
+		hanging = false;
 	}
 	
 	void Update () {
 		Screen.lockCursor = true;
+		
+		if(hanging){
+			motor.SetVelocity(Vector3.zero);
+		}
+		if(Input.GetButtonDown(jumpKey) && (motor.grounded || hanging)){
+			motor.SetVelocity(GetComponentInChildren<Camera>().ScreenPointToRay(middleScreen).direction*jumpForce);
+		}
+	}
+	void OnExternalVelocity(){
+		
 	}
 }
