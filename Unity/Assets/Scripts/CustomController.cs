3e073cd8e649316db7a9b6f11cfeb4031ff544f6
Rasmus Dyhr Larsen <ahhhnimal@gmail.com>
2013-06-08 18:52:31 +0200

    Basic Movement
    Noclip implemented. Physics coming up soon.

--- Unity/Assets/Scripts/CustomController.cs
+++ Unity/Assets/Scripts/CustomController.cs
@@ -2,23 +2,32 @@ using UnityEngine;
 using System.Collections;
 
 public class CustomController : MonoBehaviour {
+	public float MouseSensX = 1;
+	public float MouseSensY = 1;
+	public float MoveForwardSpeed = 0.5f;
+	public float MoveSidewaySpeed = 0.5f;
 	
 	private Camera cam;
 	private Vector3 face;
 
-	// Use this for initialization
 	void Start () {
 		cam = transform.GetComponentInChildren<Camera>();
-		Debug.Log(cam);
+		
+		Screen.lockCursor = true;
+	}
+	void OnMouseDown() {
+		Screen.lockCursor = true;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		// Rotate player
 		
+		// Rotate player
+		transform.Rotate(Vector3.up,Input.GetAxis("Mouse X") * MouseSensX,Space.World);
+		transform.Rotate(Vector3.left,Input.GetAxis("Mouse Y") * MouseSensY,Space.Self);
 		
 		// Move player
-		
+		transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * MoveForwardSpeed);
+		transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * MoveSidewaySpeed);
 		
 		// Message actions
 	}
