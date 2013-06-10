265c23ba20535c8398ff56ed81d4fab587d77b66
Rasmus Dyhr Larsen <ahhhnimal@gmail.com>
2013-06-10 02:16:25 +0200

    Hunter Vision
    First iteration. Might need to do some shaders, or figure out how to
    otherwise darken the screen.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,40 +2,82 @@ using UnityEngine;
 using System.Collections.Generic;
 
 public class Character : MonoBehaviour {
+	
+	// Settings
 	public string jumpKey = "Fire2";
-	public float jumpForce = 40;
+	public string visionKey = "Fire3";
 	public float maxEnergy = 100;
 	public float energyRegen = 20;
 	public float jumpCost = 20;
+	public float jumpForce = 40;
+	public Transform[] visions;
 	
+	// Jumping variables
 	private Vector3 middleScreen;
 	private CharacterMotor motor;
 	private bool hanging;
 	private bool jumping;
 	private float gravity;
-	
 	private float energy;
+	
+	// Vision variables
+	private Camera cam;
+	private int defaultMask;
+	private List<Color> defaultColor;
 
 	void Start () {
+		// Setup jumping variables
 		middleScreen = new Vector3(Screen.width/2,Screen.height/2,0);
 		motor = GetComponentInChildren<CharacterMotor>();
 		gravity = motor.movement.gravity;
 		hanging = false;
 		jumping = false;
-		
 		energy = maxEnergy;
+		
+		// Setup vision variables
+		cam = gameObject.GetComponentInChildren<Camera>();
+		defaultMask = cam.cullingMask;
+		defaultColor = new List<Color>();
+		int i = visions.Length;
+		while(i-- > 0){
+			defaultColor.Add(visions[i].renderer.material.color);
+			// TODO check if vision has hp
+		}
 	}
 	
 	void Update () {
+		// Keep locking the cursor
 		Screen.lockCursor = true;
+		
+		// Regenerate energy
 		energy += energyRegen * Time.deltaTime;
 		if(energy>maxEnergy)energy=maxEnergy;
 		
+		// Switch camera mode
+		int i = visions.Length;
+		if(Input.GetButton(visionKey)){
+			if(cam.cullingMask == defaultMask){
+				cam.cullingMask = 1 << LayerMask.NameToLayer("HunterVision");
+				while(i-- > 0){
+					visions[i].renderer.material.color = Color.red;
+				}
+			}
+		} else {
+			if(cam.cullingMask != defaultMask){
+				cam.cullingMask = defaultMask;
+				while(i-- > 0){
+					visions[i].renderer.material.color = defaultColor[i];
+				}
+			}
+		}
+		
+		// Cling to walls
 		if(hanging){
 			motor.SetVelocity(Vector3.zero);
 			motor.inputMoveDirection = Vector3.zero;
 			motor.movement.gravity = 0;
 		}
+		// Jump
 		if(Input.GetButtonDown(jumpKey) && (motor.grounded || hanging) && energy >= jumpCost){
 			hanging = false;
 			motor.movement.gravity = gravity;
@@ -46,9 +88,10 @@ public class Character : MonoBehaviour {
 		
 	}
 	void OnExternalVelocity(){
-		
+		// This is just here to stop an error. Mind it not.
 	}
 	void OnControllerColliderHit(ControllerColliderHit hit){
+		// If wall or roof collision and during jump then cling
 		if (jumping) {
 			Vector3 dir = hit.moveDirection;
 			dir.y = Mathf.Max(0,dir.y);
@@ -59,6 +102,8 @@ public class Character : MonoBehaviour {
 		jumping = false;
 	}
 	void OnGUI() {
+		// Draw energy bar
+		// TODO prettier energy bar
 		GUI.Box(new Rect(10,Screen.height-30,200+12,20),"");
 		GUI.Box(new Rect(10,Screen.height-30,(energy/maxEnergy)*200+12,20),"");
 	}
