6fdddc314234abe1b6271899c897bc6f45f3b301
Rasmus Dyhr Larsen <ahhhnimal@gmail.com>
2013-06-17 00:21:04 +0200

    Explosion Damage and Force
    Adding scaling later

--- Assets/Scripts/HunterWeapons/GrenadeObj.cs
+++ Assets/Scripts/HunterWeapons/GrenadeObj.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class GrenadeObj : MonoBehaviour {
 	public float timer = 3;
+	public float explosionRadius = 5;
+	public float explosionForce = 50;
+	public float damage = 60;
 
 	void Start () {
 	
@@ -12,11 +15,20 @@ public class GrenadeObj : MonoBehaviour {
 		timer -= Time.deltaTime;
 		if(timer <= 0){
 			GameObject[] units = GameObject.FindGameObjectsWithTag("Unit");
+			RaycastHit hit;
 			foreach(GameObject unit in units){
-				// TODO raytrace
+				if(Physics.Raycast(transform.position,unit.transform.position-transform.position,out hit,explosionRadius)){
+					if(hit.transform == unit.transform){
+						unit.GetComponentInChildren<Health>().Damage(damage);
+					}
+					if(unit.GetComponentInChildren<CharacterMotor>() != null){
+						unit.GetComponentInChildren<CharacterMotor>().SetVelocity((unit.transform.position-transform.position).normalized*explosionForce);
+					}
+				}
 			}
 			GameObject explosion = Instantiate(Resources.Load("Prefabs/Explosion")) as GameObject;
 			explosion.transform.position = transform.position;
+			Destroy(explosion,explosion.GetComponentInChildren<ParticleSystem>().duration);
 			Destroy(gameObject);
 		}
 	}
