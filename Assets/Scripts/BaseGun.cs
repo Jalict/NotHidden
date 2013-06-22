c2ab426a65333ac1fc790f24af1286b63872906b
Rasmus Dyhr Larsen <ahhhnimal@gmail.com>
2013-06-23 00:24:37 +0200

    Bullet Damage and Spread
    Also muzzle speed. UI comming up next

--- Assets/Scripts/BaseGun.cs
+++ Assets/Scripts/BaseGun.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 using System.Collections;
 
 public class BaseGun : Weapon {
+	public float spread = 0.01f;
+	public float fireRate = 30f;
+	public float muzzleSpeed = 100f;
+	public int bulletsPerShot = 1;
+	public float recoil = 1f;
+	private float timer;
+	private float fireTime;
 
 	void Start () {
 		LoadModel("Prefabs/Cube",new Vector3(0.5f,-0.25f,0.5f),Vector3.up,0.05f);
+		
+		timer = 0;
+		fireTime = 1/fireRate;
 	}
 	
 	void OnFiring () {
 		if(!enabled)return;
-		GameObject bullet = Instantiate(Resources.Load("Prefabs/Bullet")) as GameObject;
-		bullet.transform.position = cam.transform.position+cam.transform.forward*1+Vector3.down*0.5f;
-		bullet.GetComponentInChildren<Bullet>().move = cam.transform.forward*10;
+		if(timer<=0){
+			GameObject bullet = Instantiate(Resources.Load("Prefabs/Bullet")) as GameObject;
+			bullet.transform.position = cam.transform.position+cam.transform.right*0.5f-cam.transform.up*0.25f;
+			
+			Vector3 dir = cam.transform.forward;
+			dir = Vector3.RotateTowards(dir,Random.value<0.5?cam.transform.right:-cam.transform.right,Random.value*spread,0);
+			dir = Vector3.RotateTowards(dir,Random.value<0.5?cam.transform.up:-cam.transform.up,Random.value*spread,0);
+			bullet.GetComponentInChildren<Bullet>().move = dir*muzzleSpeed;
+			bullet.GetComponentInChildren<Bullet>().damage = 10;
+			
+			cam.transform.Rotate(recoil,0,0);
+			timer += fireTime;
+		} else {
+			timer -= Time.deltaTime;
+		}
 	}
 }
--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 	public Vector3 move = Vector3.zero;
+	internal float damage = 0;
 	
 	void Start () {
 	
 	}
 	
-	void FixedUpdate () {
+	void Update () {
 		RaycastHit hit;
-		if (Physics.Raycast(transform.position,move,out hit,move.magnitude*Time.fixedDeltaTime)){
+		if (Physics.Raycast(transform.position,move,out hit,move.magnitude*Time.deltaTime)){
+			if(hit.transform.GetComponentInChildren<Health>()){
+				hit.transform.GetComponentInChildren<Health>().Damage(damage);
+			}
 			Destroy(gameObject);
 		}
-		transform.position += move * Time.fixedDeltaTime;
+		transform.position += move * Time.deltaTime;
 	}
 }
